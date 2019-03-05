e0bf9e64fcbfb919efb440cde2f154d2b3f2c7e9
Cithed <43377260+Cithed@users.noreply.github.com>
2019-03-05 08:51:15 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+$("button").click(function(){
+    var answer1= $("#input1").val();
+            $("#form1").text(answer1+" "+"apples will not keep the doctor away!");
+    var answer2= $("#input2").val();
+            $("#form2").text(" "+answer2+" "+"times is not enough to stay healthy!");
+    var answer3= $("#input3").val();
+            $("#form3").text(" "+answer3+" "+"times is too many times!");
+});
